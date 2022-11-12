c0a1d7a8db920ac96668f247a19d2ce1208c8bbe
Teriyaki <103395356+PauThing@users.noreply.github.com>
2022-11-12 20:57:45 +0800

    Updates

--- BigData.R
+++ BigData.R
@@ -14,11 +14,36 @@ library(parallel)
 setwd("~/INTI College Penang - BCS/Semester 3_Aug 2022/5011CEM Big Data Programming Project/Assignment/BigDataProgramming/Dataset/area_level")
 
 #Import the dataset
+csv_files <- list.files(path = ".")
+AL_Grocery = do.call(rbind, lapply(csv_files, function(x) read.csv(x, stringsAsFactors = FALSE)))
+AL_Grocery
+
+#Sequential
 #Whole area level
-df <-
-  list.files(path = ".", pattern = "*.csv") %>%
-  map_df(~read_csv(.))
-df
+Seq <- microbenchmark("Sequential " = {do.call(rbind, lapply(list_csv_files, function(x) read.csv(x, stringsAsFactors = FALSE)))})
+Seq
+
+autoplot(Seq)
+
+#Parallel
+#Whole area level
+numCores <- detectCores()
+numCores
+
+cl <- makeCluster(numCores)
+
+clusterEvalQ(cl, 2 + 2)
+
+Par <- function(i)
+{
+  do.call(rbind, parLapply(list_csv_files, function(x) read.csv(x, stringsAsFactors = FALSE))
+}
+Par
+
+Par <- microbenchmark("Parallel " = {do.call(rbind, lapply(list_csv_files, function(x) read.csv(x, stringsAsFactors = FALSE)))})
+Par
+
+autoplot(Par)
 
 #Change directory
 setwd("~/INTI College Penang - BCS/Semester 3_Aug 2022/5011CEM Big Data Programming Project/Assignment/BigDataProgramming/Dataset")
